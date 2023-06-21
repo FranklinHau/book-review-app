cae176b7836c8f077aee6fd17be9a67db11e5b33
Franklin Hau <fhauchi@gmail.com>
2023-06-20 20:45:00 -0600

    add fetch to BookList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from './NavBar';
+import BookList from './BookList';
+import AddBook from './AddBook';
+import BookDetails from './BookDetails';
+import EditBook from './EditBook';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path='books/new' component={AddBook} />
+        <Route path='books/:id/edit' component={EditBook} />
+        <Route path='books/:id/' component={BookDetails} />
+        <Route path='books/' component={BookList} />
+      </Switch>
+    </Router>
   );
-}
+}  
 
 export default App;
