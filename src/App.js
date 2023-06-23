a472e5ec2cec7250765015afce8270624113d2c8
Franklin Hau <fhauchi@gmail.com>
2023-06-23 15:27:22 -0600

    error fix in AddBook component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //In this component, I'm using the BrowserRouter, Route, and Switch components from react-router-dom to set up routing. 
 //The NavBar component is displayed on all pages, and the Switch component is used to render only the first Route 
 //or Redirect that matches the current location.
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from './NavBar';
 import BookList from './BookList';
 import AddBook from './AddBook';
@@ -12,12 +12,12 @@ function App() {
   return (
     <Router>
       <NavBar />
-      <Switch>
+      <Routes>
         <Route path='books/new' component={AddBook} />
         <Route path='books/:id/edit' component={EditBook} />
         <Route path='books/:id/' component={BookDetails} />
         <Route path='books/' component={BookList} />
-      </Switch>
+      </Routes>
     </Router>
   );
 }
