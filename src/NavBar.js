59238d44533bec4ef38f92e531bb7e7e992901a8
Franklin Hau <fhauchi@gmail.com>
2023-06-25 22:49:39 -0600

    added a search box to NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,44 @@
 //This component will contain links to navigate between the different routes. 
 //I'm using the Link component from react-router-dom to create navigation links. 
 //When someone click on a Link, it updates the URL without causing a page refresh.
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+function BookDetails() {
+    const [books, setBooks] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('')
+
+    useEffect(() => {
+        fetch('http://localhost:3001/books')
+            .then(response => response.json())
+            .then(data => setBooks(data));
+    }, []);
+    
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredBooks = books.filter(book => 
+        book.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+
+        return (
+            <div>
+                <input 
+                    type='text'
+                    placeholder='search for a book'
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    />
+                    {filteredBooks.map(book => (
+                        <div key={book.id}>
+                            <h2><Link to={`/books/${book.id}`}>{book.title}</Link></h2>
+                            <p>{book.author}</p>
+                            </div>
+                    ))}
+            </div>
+        );
+}
 
 function NavBar() {
     return (
